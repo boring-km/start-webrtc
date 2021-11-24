0258430ea72bd2e495b0ca727847ce403d580c83
boring-km <kms0644804@naver.com>
2021-11-24 22:36:06 +0900

    [KM] react 프로젝트 초기화

---
+++ km/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import WebVideo from "./WebVideo";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Just App Header</h2>
+      </header>
+      <WebVideo name={'진강민'} />
+    </div>
+  );
+}
+
+export default App;

5497475894f184d839314b2bcdbbdc5e8467592f
boring-km <kms0644804@naver.com>
2021-11-27 22:11:39 +0900

    [KM] ssl 로컬 키 삭제

--- km/src/App.tsx
+++ km/src/App.tsx
@@ -14,7 +14,7 @@ const pc_config = {
   ],
 };
 
-const SOCKET_SERVER_URL = "http://localhost:8080";
+const SOCKET_SERVER_URL = "https://localhost:8080";
 
 const App = () => {
   const socketRef = useRef<SocketIOClient.Socket>();
